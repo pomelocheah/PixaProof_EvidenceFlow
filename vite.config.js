b07f1fe628743ea84c933a21d30e91d5456af9f1
Jinyang326 <jinyangwong3839@gmail.com>
2026-09-25 00:07:40 +0800

    Use clean public assets for Pages build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/PixaProof_EvidenceFlow/' : '/',
+  publicDir: 'public-deploy',
   plugins: [react()],
   server: {
     proxy: {
