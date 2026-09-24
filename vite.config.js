92b945b60e73a1ebfa53fa54a660f5d9a0beaf33
Jinyang326 <jinyangwong3839@gmail.com>
2026-09-25 00:59:05 +0800

    Allow local tunnel hosts for demos

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   publicDir: 'public-deploy',
   plugins: [react()],
   server: {
+    allowedHosts: true,
     proxy: {
       '/api': 'http://127.0.0.1:5177',
     },
